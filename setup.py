d68a3c0936069d86a8cd0eb9f36d4902d114212b
Tomasz Rybarczyk <paluho@gmail.com>
2013-08-11 16:48:42 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='rrd-cdef',
     author='Tomasz Rybarczyk',
     author_email='paluho@gmail.com',
-    version = '2012.12.1',
+    version = '2013.08.1',
     url='http://github.com/paluh/rrd-cdef',
     py_modules=['cdef'],
     description='Trivial DSL for RRD CDEFs.',
