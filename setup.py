2eda2e77dc5a11a98a784267fc2e2a3536f07699
Tomasz Rybarczyk <paluho@gmail.com>
2013-08-25 17:00:30 +0200

    Add pytz to requirements

--- setup.py
+++ setup.py
@@ -3,14 +3,19 @@ try:
 except ImportError:
     from distutils.core import setup
 
+REQUIREMENTS = [
+    'pytz',
+]
+
 setup(
     name='rrd-cdef',
     author='Tomasz Rybarczyk',
     author_email='paluho@gmail.com',
     version = '2013.08.1',
     url='http://github.com/paluh/rrd-cdef',
-    py_modules=['cdef'],
     description='Trivial DSL for RRD CDEFs.',
+    py_modules=['cdef'],
+    install_requires=REQUIREMENTS,
     test_suite='tests',
     zip_safe=False,
     classifiers=[
